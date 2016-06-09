daf2452a88a4c35f706ced46e95886b6ddb3f3a4
neumanmy <mandy.neumann@gesis.org>
2016-06-09 13:28:25 +0200

    adds test for conversion methods

--- YetToBeNamed/src/test/java/util/input/DocReadingTest.java
+++ YetToBeNamed/src/test/java/util/input/DocReadingTest.java
@@ -2,46 +2,60 @@ package util.input;
 
 import java.io.File;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import datamodel.Document;
+import util.convert.Converter;
 import util.convert.PDFConverter;
 
 public class DocReadingTest {
 
-	private static final String DOCLOCATION = null;
+	private static String docLocation;
+	private static String shortPaperName;
+	private static String longPaperName;
 
 	@Before
 	public void setUp() throws Exception {
-
+		docLocation = "/pdf/";
+		shortPaperName = "2819.pdf";
+		longPaperName = "29294.pdf";
 	}
 
 	@Test
 	public void testReadSingleFile() {
-		File docFile = new File(DOCLOCATION);
-		Document doc = PDFConverter.convert(docFile);
+		File docFile = getFile(docLocation + shortPaperName);
+		Document doc = PDFConverter.convert(docFile, Converter.JPOD);
+		Assert.assertFalse(doc.isEmpty());
 	}
 
 	@Test
 	public void testReadSinglePath() {
-		Path docPath = Paths.get(DOCLOCATION);
-		Document doc = PDFConverter.convert(docPath);
-	}
-
-	@Test
-	public void testReadFromDir() {
-
+		Path docPath = getFile(shortPaperName).toPath();
+		Document doc = PDFConverter.convert(docPath, Converter.JPOD);
+		Assert.assertFalse(doc.isEmpty());
 	}
 
 	@Test
 	public void testReadMultipleFiles() {
 		List<Path> docPaths = new ArrayList<Path>();
-		List<Document> docs = PDFConverter.convert(docPaths);
+
+		List<Document> docs = PDFConverter.convert(docPaths, Converter.JPOD);
+
+		Assert.assertFalse(docs.isEmpty());
+
+		for (Document document : docs) {
+			Assert.assertFalse(document.isEmpty());
+		}
+
+	}
+
+	File getFile(String relativePath) {
+		return new File(getClass().getResource(relativePath).getFile());
 	}
 
 }
