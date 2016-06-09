e25f3ec78ee631025641b9069076820467fa3aa4
neumanmy <mandy.neumann@gesis.org>
2016-06-09 11:32:10 +0200

    adds empty converter methods

--- YetToBeNamed/src/main/java/util/convert/PDFConverter.java
+++ YetToBeNamed/src/main/java/util/convert/PDFConverter.java
@@ -1,5 +1,26 @@
 package util.convert;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.util.List;
+
+import datamodel.Document;
+
 public class PDFConverter {
+
+	public static Document convert(File docFile) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static Document convert(Path docPath) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static List<Document> convert(List<Path> docPaths) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
