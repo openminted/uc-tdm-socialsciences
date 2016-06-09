f8d8be25064dd4bd5bb651dd87ec9091418c7277
neumanmy <mandy.neumann@gesis.org>
2016-06-09 11:32:19 +0200

    adds tests for converter methods

--- YetToBeNamed/src/test/java/util/input/DocReadingTest.java
+++ YetToBeNamed/src/test/java/util/input/DocReadingTest.java
@@ -1,19 +1,47 @@
 package util.input;
 
-import static org.junit.Assert.*;
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import datamodel.Document;
+import util.convert.PDFConverter;
+
 public class DocReadingTest {
+
+	private static final String DOCLOCATION = null;
 
 	@Before
 	public void setUp() throws Exception {
+
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testReadSingleFile() {
+		File docFile = new File(DOCLOCATION);
+		Document doc = PDFConverter.convert(docFile);
+	}
+
+	@Test
+	public void testReadSinglePath() {
+		Path docPath = Paths.get(DOCLOCATION);
+		Document doc = PDFConverter.convert(docPath);
+	}
+
+	@Test
+	public void testReadFromDir() {
+
+	}
+
+	@Test
+	public void testReadMultipleFiles() {
+		List<Path> docPaths = new ArrayList<Path>();
+		List<Document> docs = PDFConverter.convert(docPaths);
 	}
 
 }
