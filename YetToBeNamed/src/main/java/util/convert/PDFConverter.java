cb1b42bcfdf9eb952b0a719b93c793ff27aca4fd
neumanmy <mandy.neumann@gesis.org>
2016-06-09 13:28:09 +0200

    adds conversion methods

--- YetToBeNamed/src/main/java/util/convert/PDFConverter.java
+++ YetToBeNamed/src/main/java/util/convert/PDFConverter.java
@@ -2,25 +2,63 @@ package util.convert;
 
 import java.io.File;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 import datamodel.Document;
 
 public class PDFConverter {
 
-	public static Document convert(File docFile) {
-		// TODO Auto-generated method stub
-		return null;
+	public static Document convert(File docFile, Converter converter) {
+		return convert(docFile.toPath(), converter);
 	}
 
-	public static Document convert(Path docPath) {
-		// TODO Auto-generated method stub
-		return null;
+	public static Document convert(Path docPath, Converter converter) {
+		Document doc = new Document(docPath.getFileName().toString());
+
+		switch (converter) {
+		case GROBID:
+			convertWithGrobid(docPath, doc);
+			break;
+		case JPOD:
+			convertWithJPod(docPath, doc);
+			break;
+		case PDFBOX:
+			convertWithPdfBox(docPath, doc);
+			break;
+		case TIKA:
+			convertWithTika(docPath, doc);
+			break;
+		default:
+			break;
+		}
+
+		return doc;
 	}
 
-	public static List<Document> convert(List<Path> docPaths) {
-		// TODO Auto-generated method stub
-		return null;
+	private static void convertWithTika(Path docPath, Document doc) {
+
+	}
+
+	private static void convertWithPdfBox(Path docPath, Document doc) {
+
+	}
+
+	private static void convertWithJPod(Path docPath, Document doc) {
+
+	}
+
+	private static void convertWithGrobid(Path docPath, Document doc) {
+
+	}
+
+	public static List<Document> convert(List<Path> docPaths, Converter converter) {
+		List<Document> result = new ArrayList<Document>();
+
+		for (Path path : docPaths) {
+			result.add(convert(path, converter));
+		}
+		return result;
 	}
 
 }
