effd89dc16507e5cc061ea94725a33bb9783416c
neumanmy <mandy.neumann@gesis.org>
2016-06-14 17:06:27 +0200

    adds test method for reading study data

--- YetToBeNamed/src/test/java/util/input/StudyReaderTest.java
+++ YetToBeNamed/src/test/java/util/input/StudyReaderTest.java
@@ -1,27 +1,19 @@
 package util.input;
 
-import static org.junit.Assert.fail;
-
 import org.junit.Before;
 import org.junit.Test;
 
 public class StudyReaderTest {
 
-	private String inputURL;
-	private String DATASET_URI;
-	private String STUDY_URI;
-
 	@Before
 	public void setUp() throws Exception {
-		inputURL = "http://zacat.gesis.org/obj/fCatalog/ZACAT@datasets";
 
-		DATASET_URI = "http://zacat.gesis.org:80/obj/fCatalog/ZACAT@datasets";
-		STUDY_URI = "http://zacat.gesis.org:80/obj/fStudy/";
 	}
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		StudyReader reader = new StudyReader();
+		reader.read();
 	}
 
 }
