119911b0b303b232bf65734e86c99a14bcad0101
neumanmy <mandy.neumann@gesis.org>
2016-06-14 16:18:41 +0200

    adds constructor

--- YetToBeNamed/src/main/java/util/input/StudyReader.java
+++ YetToBeNamed/src/main/java/util/input/StudyReader.java
@@ -1,5 +1,14 @@
 package util.input;
 
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+
 public class StudyReader {
+
+	private Model model;
+
+	public StudyReader() {
+		model = ModelFactory.createDefaultModel();
+	}
 
 }
