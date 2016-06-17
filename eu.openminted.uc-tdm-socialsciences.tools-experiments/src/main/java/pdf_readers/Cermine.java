91f344e9fd5674b136030b14c0f7c99c3482a237
maxxkia <masoud.kiaeeha@gmail.com>
2016-06-17 12:25:49 +0200

    No issue. Created CERMINE usage example.
    - added repository entry for CERMINE artifact in
    tools-experiments/pom.xml
    - added first example for CERMINE usage

--- eu.openminted.uc-tdm-socialsciences.tools-experiments/src/main/java/pdf_readers/Cermine.java
+++ eu.openminted.uc-tdm-socialsciences.tools-experiments/src/main/java/pdf_readers/Cermine.java
@@ -1,6 +1,49 @@
 package pdf_readers;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+
+import org.jdom.Element;
+import org.jdom.output.XMLOutputter;
+
+import pl.edu.icm.cermine.PdfNLMContentExtractor;
+import pl.edu.icm.cermine.exception.AnalysisException;
+
 public class Cermine
 {
+    private static final String DATASET_PATH = "/home/local/UKP/kiaeeha/workspace/Datasets/openminted/uc-ss/pdf/";
+    private static final String TEST_PDF_RESOURCE = "2819.pdf";
+    private static final String OUTPUT_RESOURCE = "target/2819.txt";
 
+    public static void main(String args[])
+        throws AnalysisException, IOException
+    {
+        PdfNLMContentExtractor extractor = new PdfNLMContentExtractor();
+        extractor.setExtractText(true);
+        extractor.setExtractText(true);
+        InputStream inputStream = new FileInputStream(DATASET_PATH + TEST_PDF_RESOURCE);
+        Element result = extractor.extractContent(inputStream);
+
+        OutputStreamWriter outputStream = new OutputStreamWriter(
+                new FileOutputStream(OUTPUT_RESOURCE), "UTF-8");
+        XMLOutputter outputter = new XMLOutputter();
+        outputter.output(result, outputStream);
+    }
+
+    @SuppressWarnings("unused")
+    private static void printChildrenContents(Element result)
+    {
+        for (Object child : result.getChildren()) {
+            Element childE = (Element) child;
+            if (childE.getChildren().size() == 0) {
+                System.out.println(childE.getValue());
+            }
+            else {
+                printChildrenContents(childE);
+            }
+        }
+    }
 }
