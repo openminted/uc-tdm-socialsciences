0be688f68300fe36b16f07bce2162fc8de42827a
neumanmy <mandy.neumann@gesis.org>
2016-06-22 10:10:49 +0200

    small adjustments on tests

--- YetToBeNamed/src/test/java/util/output/DBWriterTest.java
+++ YetToBeNamed/src/test/java/util/output/DBWriterTest.java
@@ -1,13 +1,16 @@
 package util.output;
 
 import java.sql.SQLException;
+import java.util.Set;
 
+import org.apache.jena.rdf.model.ResourceFactory;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import datamodel.Dataset;
 import datamodel.Variable;
+import util.input.StudyReader;
 
 public class DBWriterTest {
 
@@ -50,6 +53,30 @@ public class DBWriterTest {
 	public void testWriteVariable() throws SQLException {
 		writer.write(v1, 1);
 		writer.write(v2, 1);
+	}
+
+	@Test
+	public void testWriteSingleRealData() {
+		StudyReader reader = new StudyReader();
+
+		Dataset ds = reader
+				.followDataset(ResourceFactory.createResource("http://zacat.gesis.org:80/obj/fStudy/ZA3779"));
+		writer.write(ds);
+
+		Set<Variable> variables = ds.getVariables();
+		for (Variable variable : variables) {
+			writer.write(variable, ds.getId());
+		}
+	}
+
+	@Test
+	public void testWriteAllRealData() {
+		StudyReader reader = new StudyReader();
+
+		Set<Dataset> datasets = reader.read();
+		for (Dataset dataset : datasets) {
+			writer.write(dataset);
+		}
 	}
 
 	@AfterClass
