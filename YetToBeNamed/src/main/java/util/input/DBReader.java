554099b694036fbb312e9939d161fd6c5d7bb1ce
neumanmy <mandy.neumann@gesis.org>
2016-06-23 09:51:06 +0200

    adds class for reading DB and corresponding TestCase

---
+++ YetToBeNamed/src/main/java/util/input/DBReader.java
@@ -0,0 +1,5 @@
+package util.input;
+
+public class DBReader {
+
+}
---
+++ YetToBeNamed/src/test/java/util/input/DBReaderTest.java
@@ -0,0 +1,14 @@
+package util.input;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DBReaderTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
