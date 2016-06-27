0b9fe00b4b340289668014fec6c0e99f8cdbcf2c
neumanmy <mandy.neumann@gesis.org>
2016-06-27 12:59:37 +0200

    adds test for db reader

--- YetToBeNamed/src/test/java/util/input/DBReaderTest.java
+++ YetToBeNamed/src/test/java/util/input/DBReaderTest.java
@@ -1,14 +1,27 @@
 package util.input;
 
-import static org.junit.Assert.*;
+import java.util.Set;
 
+import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import datamodel.Dataset;
 
 public class DBReaderTest {
 
+	private static DBReader reader;
+
+	@BeforeClass
+	public static void setUpBeforeClass() {
+		reader = new DBReader("test.sqlite");
+	}
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testRead() {
+		Set<Dataset> readData = reader.readData();
+
+		Assert.assertTrue(readData.isEmpty());
 	}
 
 }
