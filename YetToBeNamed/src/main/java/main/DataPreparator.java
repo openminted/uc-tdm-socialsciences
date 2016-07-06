5e60693304f78cc31a86b97e08be1aed0f52716e
neumanmy <mandy.neumann@gesis.org>
2016-07-06 13:03:52 +0200

    separates methods to read in data from several sources

--- YetToBeNamed/src/main/java/main/DataPreparator.java
+++ YetToBeNamed/src/main/java/main/DataPreparator.java
@@ -1,14 +1,11 @@
 package main;
 
 import java.nio.file.Paths;
-import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Set;
 
 import datamodel.Document;
 import eval.GoldData;
-import eval.Reference;
 import util.input.DocReader;
 import util.input.GoldDataReader;
 import util.input.StudyReader;
@@ -18,20 +15,17 @@ public class DataPreparator {
 
 	public static void main(String[] args) {
 
-		/*
-		 * TODO: die vorbereitenden Methoden auslagern
-		 */
+		fillStudyDB();
 
+		fillLabeledDataDB();
+
+		fillDocumentsDB();
 		/*
 		 * TODO: in GoldData weiß ich, welches Dokument welche Studie behandelt
 		 * (indirekt). Dieses Wissen nutzen, um gezielt die Variablen aus der
 		 * Studie im Dokument zu finden. Schlüssel: Dokumentname.
 		 *
-		 * Evtl. da auch noch was im StudyReader hinzufügen, was mir die
-		 * Variablen on the fly holt, weil die DB wegen Heap Overflow nicht
-		 * vollständig ist.
-		 *
-		 * neuer Workflow:
+		 * alternativer Workflow:
 		 *
 		 * erst gelabelte Daten einlesen
 		 *
@@ -44,44 +38,33 @@ public class DataPreparator {
 		 *
 		 * jeweils über IDs Verknüpfungen herstellen
 		 */
+	}
 
-		/*
-		 * read study data from RDF into DB
-		 */
-		StudyReader studyReader = new StudyReader(DBManager.getInstance(false).createTables());
-		studyReader.read(-1);
-
-		/*
-		 * read labeled data from Excel (xlsx) into DB
-		 */
-		GoldDataReader goldReader = new GoldDataReader(Paths.get(ProjectConstants.goldDataPath));
-		Set<GoldData> goldData = goldReader.readData();
-		System.out.println("Number of gold data: " + goldData.size());
-
-		/*
-		 * read documents from PDF to text and into DB
-		 */
+	/*
+	 * TODO read documents from PDF to text and into DB
+	 */
+	private static void fillDocumentsDB() {
 		DocReader docReader = new DocReader(Paths.get(ProjectConstants.docFolder));
 		Map<String, Document> documents = docReader.readDocuments();
 		System.out.println("Number of documents: " + documents.size());
 
-		for (GoldData labeled : goldData) {
-			String datasetID = labeled.getDatasetID();
-			Map<String, List<Reference>> references = labeled.getReferences();
+	}
 
-			studyReader.readDataset(datasetID);
+	/*
+	 * TODO read labeled data from Excel (xlsx) into DB
+	 */
+	private static void fillLabeledDataDB() {
 
-			for (Entry<String, List<Reference>> varRefs : references.entrySet()) {
-				String varName = varRefs.getKey();
+		GoldDataReader goldReader = new GoldDataReader(Paths.get(ProjectConstants.goldDataPath));
+		Set<GoldData> goldData = goldReader.readData();
+		System.out.println("Number of gold data: " + goldData.size());
+	}
 
-				// TODO: query db for var
-
-				List<Reference> refs = varRefs.getValue();
-				for (Reference reference : refs) {
-					reference.getPaperID();
-					reference.getReferenceText();
-				}
-			}
-		}
+	/*
+	 * read study data from RDF into DB
+	 */
+	private static void fillStudyDB() {
+		StudyReader studyReader = new StudyReader(DBManager.getInstance(false).createTables());
+		studyReader.read(-1);
 	}
 }
