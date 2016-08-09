f753ac954961697afff04ccf08ec79f8f394d1cb
neumanmy <mandy.neumann@gesis.org>
2016-08-09 16:34:39 +0200

    Saving files before refreshing line endings

--- ss-io-pdfx-xml/src/main/java/eu/openminted/uc_tdm_socialsciences/io/pdfx/PdfxXmlToXmiConverter.java
+++ ss-io-pdfx-xml/src/main/java/eu/openminted/uc_tdm_socialsciences/io/pdfx/PdfxXmlToXmiConverter.java
@@ -1,19 +1,20 @@
 package eu.openminted.uc_tdm_socialsciences.io.pdfx;
 
-import de.tudarmstadt.ukp.dkpro.core.io.xmi.XmiWriter;
-import de.tudarmstadt.ukp.dkpro.core.tokit.BreakIteratorSegmenter;
-import org.apache.uima.UIMAException;
-
-import java.io.IOException;
-
 import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
 import static org.apache.uima.fit.factory.CollectionReaderFactory.createReaderDescription;
 import static org.apache.uima.fit.pipeline.SimplePipeline.runPipeline;
 
+import java.io.IOException;
+
+import org.apache.uima.UIMAException;
+
+import de.tudarmstadt.ukp.dkpro.core.io.xmi.XmiWriter;
+import de.tudarmstadt.ukp.dkpro.core.tokit.BreakIteratorSegmenter;
+
 public class PdfxXmlToXmiConverter {
     //todo: extract these hardcoded input/output paths
-    public static final String INPUT_RESOURCE_DIR = "ss-io-pdfx-xml/src/test/resources/";
-    public static final String OUTPUT_RESOURCE_DIR = "ss-io-pdfx-xml/target/";
+	public static final String INPUT_RESOURCE_DIR = "src/test/resources/";
+	public static final String OUTPUT_RESOURCE_DIR = "target/";
 
     public static final String[] RESOURCE_NAMES = {"2819", "27940"};
 
