e783e7121dbe19094fb8d58ea8b5c2ce3e0e1561
neumanmy <mandy.neumann@gesis.org>
2016-08-17 16:15:19 +0200

    Generalizes test cases to work with all resources (removes hard-coded file names)

--- ss-io-pdfx-xml/src/test/java/eu/openminted/uc_tdm_socialsciences/io/pdfx/PdfxXmlReaderTest.java
+++ ss-io-pdfx-xml/src/test/java/eu/openminted/uc_tdm_socialsciences/io/pdfx/PdfxXmlReaderTest.java
@@ -1,115 +1,104 @@
 package eu.openminted.uc_tdm_socialsciences.io.pdfx;
 
-import de.tudarmstadt.ukp.dkpro.core.testing.DkproTestContext;
-import de.tudarmstadt.ukp.dkpro.core.testing.dumper.CasDumpWriter;
+import static de.tudarmstadt.ukp.dkpro.core.testing.IOTestRunner.testOneWay;
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
+import static org.apache.uima.fit.factory.CollectionReaderFactory.createReaderDescription;
+import static org.apache.uima.fit.pipeline.SimplePipeline.runPipeline;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 
-import static de.tudarmstadt.ukp.dkpro.core.testing.IOTestRunner.testOneWay;
-import static org.apache.uima.fit.factory.CollectionReaderFactory.createReaderDescription;
-
-import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
-import static org.apache.uima.fit.pipeline.SimplePipeline.runPipeline;
+import de.tudarmstadt.ukp.dkpro.core.testing.DkproTestContext;
+import de.tudarmstadt.ukp.dkpro.core.testing.dumper.CasDumpWriter;
 
 public class PdfxXmlReaderTest {
-    public static final String TEST_RESOURCES_PATH = "src/test/resources/";
+	public static final String TEST_RESOURCES_PATH = "src/test/resources/";
 
-    public static final String TEST_RESOURCE_ARTICLE1 = "2819-pdfx.xml";
-    public static final String TEST_RESOURCE_ARTICLE2 = "27939-pdfx.xml";
+	public static final String TEST_RESOURCE_ARTICLE1 = "2819-pdfx.xml";
 
-    public static final String TEST_RESOURCE_ARTICLE1_PATH = TEST_RESOURCES_PATH + TEST_RESOURCE_ARTICLE1;
-    public static final String TEST_RESOURCE_ARTICLE2_PATH = TEST_RESOURCES_PATH + TEST_RESOURCE_ARTICLE2;
+	public static final String TEST_RESOURCE_ARTICLE1_APPENDED_XML_DUMP = "2819-pdfx-appended.xml.dump";
+	public static final String TEST_RESOURCE_ARTICLE2_APPENDED_XML_DUMP = "27939-pdfx-appended.xml.dump";
 
-    public static final String TEST_RESOURCE_ARTICLE1_DUMP = "2819-pdfx.xml.dump";
-    public static final String TEST_RESOURCE_ARTICLE2_DUMP = "27939-pdfx.xml.dump";
+	public static final String TEST_RESOURCE_ARTICLE1_APPENDED_XML_DUMP_PATH = TEST_RESOURCES_PATH
+			+ TEST_RESOURCE_ARTICLE1_APPENDED_XML_DUMP;
+	public static final String TEST_RESOURCE_ARTICLE2_APPENDED_XML_DUMP_PATH = TEST_RESOURCES_PATH
+			+ TEST_RESOURCE_ARTICLE2_APPENDED_XML_DUMP;
 
-    public static final String TEST_RESOURCE_ARTICLE1_DUMP_PATH = TEST_RESOURCES_PATH + TEST_RESOURCE_ARTICLE1_DUMP;
-    public static final String TEST_RESOURCE_ARTICLE2_DUMP_PATH = TEST_RESOURCES_PATH + TEST_RESOURCE_ARTICLE2_DUMP;
+	public PdfxXmlReaderTest() {
 
-    public static final String TEST_RESOURCE_ARTICLE1_APPENDED_XML_DUMP = "2819-pdfx-appended.xml.dump";
-    public static final String TEST_RESOURCE_ARTICLE2_APPENDED_XML_DUMP = "27939-pdfx-appended.xml.dump";
+	}
 
-    public static final String TEST_RESOURCE_ARTICLE1_APPENDED_XML_DUMP_PATH = TEST_RESOURCES_PATH +
-            TEST_RESOURCE_ARTICLE1_APPENDED_XML_DUMP;
-    public static final String TEST_RESOURCE_ARTICLE2_APPENDED_XML_DUMP_PATH = TEST_RESOURCES_PATH +
-            TEST_RESOURCE_ARTICLE2_APPENDED_XML_DUMP;
+	@Test
+	public void testReadArticles() throws Exception {
 
-    public PdfxXmlReaderTest(){
+		List<Path> xmlFiles = getXmlFilesFromDir(Paths.get(TEST_RESOURCES_PATH));
 
-    }
+		for (Path xml : xmlFiles) {
+			String filePath = xml.toString();
+			String expectedFilePath = xml.toString() + ".dump";
 
-    @Test
-    public void testReadArticle1() throws Exception
-    {
-        testOneWay(
-                createReaderDescription(PdfxXmlReader.class,
-                        PdfxXmlReader.PARAM_LANGUAGE, "en"),
-                TEST_RESOURCE_ARTICLE1_DUMP,
-                TEST_RESOURCE_ARTICLE1);
+			System.out.println(filePath);
+			System.out.println(expectedFilePath);
 
-        //After applying a change to the reader that changes its underlying jcas, this part should be uncommented
-        // and run once to create a new dump test resource file to be used in one-way test
-/*
-        runPipeline(
-                createReaderDescription(PdfxXmlReader.class,
-                        PdfxXmlReader.PARAM_LANGUAGE, "en",
-                        PdfxXmlReader.PARAM_SOURCE_LOCATION, TEST_RESOURCE_ARTICLE1_PATH),
-                createEngineDescription(CasDumpWriter.class,
-                        CasDumpWriter.PARAM_TARGET_LOCATION, TEST_RESOURCE_ARTICLE1_DUMP_PATH,
-                        CasDumpWriter.PARAM_SORT, true)
-        );
-*/
-    }
+			runPipeline(
+					createReaderDescription(PdfxXmlReader.class, PdfxXmlReader.PARAM_LANGUAGE, "en",
+							PdfxXmlReader.PARAM_SOURCE_LOCATION, filePath),
+					createEngineDescription(CasDumpWriter.class, CasDumpWriter.PARAM_TARGET_LOCATION, expectedFilePath,
+							CasDumpWriter.PARAM_SORT, true));
 
-    @Test
-    public void testReadArticle2() throws Exception
-    {
-        testOneWay(
-                createReaderDescription(PdfxXmlReader.class,
-                        PdfxXmlReader.PARAM_LANGUAGE, "en"),
-                TEST_RESOURCE_ARTICLE2_DUMP,
-                TEST_RESOURCE_ARTICLE2);
+			String fileName = xml.getFileName().toString();
+			String expectedFileName = fileName + ".dump";
 
-        //After applying a change to the reader that changes its underlying jcas, this part should be uncommented
-        // and run once to create a new dump test resource file to be used in one-way test
-/*
-        runPipeline(
-                createReaderDescription(PdfxXmlReader.class,
-                        PdfxXmlReader.PARAM_LANGUAGE, "en",
-                        PdfxXmlReader.PARAM_SOURCE_LOCATION, TEST_RESOURCE_ARTICLE2_PATH),
-                createEngineDescription(CasDumpWriter.class,
-                        CasDumpWriter.PARAM_TARGET_LOCATION, TEST_RESOURCE_ARTICLE2_DUMP_PATH,
-                        CasDumpWriter.PARAM_SORT, true)
-        );
-*/
-    }
+			System.out.println(fileName);
+			System.out.println(expectedFileName);
 
-    @Ignore
-    public void testReadArticle1WithAppendNewLine() throws Exception
-    {
-        testOneWay(
-                createReaderDescription(PdfxXmlReader.class,
-                        PdfxXmlReader.PARAM_LANGUAGE, "en",
-                        PdfxXmlReader.PARAM_APPEND_NEW_LINE_AFTER_PARAGRAPH, true),
-                TEST_RESOURCE_ARTICLE1_APPENDED_XML_DUMP,
-                TEST_RESOURCE_ARTICLE1);
+			testOneWay(createReaderDescription(PdfxXmlReader.class, PdfxXmlReader.PARAM_LANGUAGE, "en"),
+					expectedFileName, fileName);
+		}
+	}
 
-        //After applying a change to the reader that changes its underlying jcas, this part should be uncommented
-        // and run once to create a new dump test resource file to be used in one-way test
-/*
-        runPipeline(
-                createReaderDescription(PdfxXmlReader.class,
-                        PdfxXmlReader.PARAM_LANGUAGE, "en",
-                        PdfxXmlReader.PARAM_SOURCE_LOCATION, TEST_RESOURCE_ARTICLE1_PATH,
-                        PdfxXmlReader.PARAM_APPEND_NEW_LINE_AFTER_PARAGRAPH, true),
-                createEngineDescription(CasDumpWriter.class,
-                        CasDumpWriter.PARAM_TARGET_LOCATION, TEST_RESOURCE_ARTICLE1_APPENDED_XML_DUMP_PATH,
-                        CasDumpWriter.PARAM_SORT, true)
-        );
-*/
-    }
+	private static List<Path> getXmlFilesFromDir(Path inputDir) {
+		List<Path> toProcess = new ArrayList<>();
+		try {
+			Files.walk(inputDir).filter(Files::isRegularFile).filter((p) -> p.toString().endsWith(".xml"))
+					.forEach(toProcess::add);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return toProcess;
+	}
 
-    @Rule
-    public DkproTestContext testContext = new DkproTestContext();
+	@Ignore
+	public void testReadArticle1WithAppendNewLine() throws Exception {
+		testOneWay(
+				createReaderDescription(PdfxXmlReader.class, PdfxXmlReader.PARAM_LANGUAGE, "en",
+						PdfxXmlReader.PARAM_APPEND_NEW_LINE_AFTER_PARAGRAPH, true),
+				TEST_RESOURCE_ARTICLE1_APPENDED_XML_DUMP, TEST_RESOURCE_ARTICLE1);
+
+		// After applying a change to the reader that changes its underlying
+		// jcas, this part should be uncommented
+		// and run once to create a new dump test resource file to be used in
+		// one-way test
+		/*
+		 * runPipeline( createReaderDescription(PdfxXmlReader.class,
+		 * PdfxXmlReader.PARAM_LANGUAGE, "en",
+		 * PdfxXmlReader.PARAM_SOURCE_LOCATION, TEST_RESOURCE_ARTICLE1_PATH,
+		 * PdfxXmlReader.PARAM_APPEND_NEW_LINE_AFTER_PARAGRAPH, true),
+		 * createEngineDescription(CasDumpWriter.class,
+		 * CasDumpWriter.PARAM_TARGET_LOCATION,
+		 * TEST_RESOURCE_ARTICLE1_APPENDED_XML_DUMP_PATH,
+		 * CasDumpWriter.PARAM_SORT, true) );
+		 */
+	}
+
+	@Rule
+	public DkproTestContext testContext = new DkproTestContext();
 }
