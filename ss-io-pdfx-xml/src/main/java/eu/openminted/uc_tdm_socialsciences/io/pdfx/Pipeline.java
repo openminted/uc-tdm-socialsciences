27eef23d0cc119ad3d4c3274174074e9db5e785d
neumanmy <mandy.neumann@gesis.org>
2016-08-23 13:48:58 +0200

    Adds small pipeline application for pdf->xmi creation

---
+++ ss-io-pdfx-xml/src/main/java/eu/openminted/uc_tdm_socialsciences/io/pdfx/Pipeline.java
@@ -0,0 +1,34 @@
+package eu.openminted.uc_tdm_socialsciences.io.pdfx;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+
+import org.apache.uima.UIMAException;
+
+public class Pipeline {
+
+	public static void main(String[] args) {
+		if (args.length != 1) {
+			System.out.println("Please provide path to input directory containing pdf files.");
+			System.exit(0);
+		}
+
+		String inputPath = args[0];
+
+		PdfxXmlCreator pdfxXmlCreator = new PdfxXmlCreator();
+		pdfxXmlCreator.setOverwriteOutput(true);
+		try {
+			List<Path> pdfxOutFiles = pdfxXmlCreator.process(inputPath, inputPath);
+
+			for (Path p : pdfxOutFiles) {
+				PdfxXmlToXmiConverter.convert(p.toString(), p.toString() + ".xmi");
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (UIMAException e) {
+			e.printStackTrace();
+		}
+	}
+}
