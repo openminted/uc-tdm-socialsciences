5ba8bf5d1f0d29f45e27977eee73bf6a682395f0
neumanmy <mandy.neumann@gesis.org>
2016-12-19 10:35:19 +0100

    Extend pipeline.
    Now can make use of the types defined in custom type system.
    Wrapped pipeline code in try-catch block.
    Temporary solution: boolean variable specifies if should work with pre-defined or custom model.

--- ss-module-ner/src/main/java/eu/openminted/uc/socialsciences/ner/main/Pipeline.java
+++ ss-module-ner/src/main/java/eu/openminted/uc/socialsciences/ner/main/Pipeline.java
@@ -1,47 +1,136 @@
 package eu.openminted.uc.socialsciences.ner.main;
 
-import static org.apache.uima.fit.factory.CollectionReaderFactory.createReaderDescription;
 import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
+import static org.apache.uima.fit.factory.CollectionReaderFactory.createReaderDescription;
 import static org.apache.uima.fit.pipeline.SimplePipeline.runPipeline;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.apache.uima.UIMAException;
+import org.apache.uima.analysis_engine.AnalysisEngine;
 import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.analysis_engine.metadata.SofaMapping;
 import org.apache.uima.collection.CollectionReaderDescription;
+import org.apache.uima.fit.factory.AnalysisEngineFactory;
+import org.apache.uima.fit.factory.FlowControllerFactory;
+import org.apache.uima.fit.factory.TypeSystemDescriptionFactory;
+import org.apache.uima.flow.FlowController;
+import org.apache.uima.flow.FlowControllerDescription;
+import org.apache.uima.resource.ExternalResourceDescription;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.apache.uima.resource.metadata.ConfigurationParameter;
+import org.apache.uima.resource.metadata.TypeDescription;
+import org.apache.uima.resource.metadata.TypePriorities;
+import org.apache.uima.resource.metadata.TypeSystemDescription;
+import org.apache.uima.util.CasCreationUtils;
 
 import de.tudarmstadt.ukp.dkpro.core.io.xmi.XmiReader;
 import de.tudarmstadt.ukp.dkpro.core.io.xmi.XmiWriter;
-import de.tudarmstadt.ukp.dkpro.core.io.bincas.BinaryCasReader;
 import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordNamedEntityRecognizer;
+import eu.openminted.uc.socialsciences.ner.helper.BinaryCasToStanfordTsvConverter;
 
 public class Pipeline {
 
-	public static void main(String[] args) throws Exception {
-		String inputPattern = "./**/*.bin";
-		String modelLocation = "omtd-ner-model.ser.gz";
-		/*
-		 * question: param language? do we make use of it? do we assume separate
-		 * models for EN and DE?
-		 */
-		String language = "en";
+	private static final Logger logger = Logger.getLogger(Pipeline.class);
 
-		CollectionReaderDescription reader = createReaderDescription(XmiReader.class,
-				XmiReader.PARAM_SOURCE_LOCATION, inputPattern,
-				XmiReader.PARAM_LANGUAGE, language);
+	public static void main(String[] args) {
+		String inputPattern = "./**/*.xmi";
+		String modelLocation = "omtd-ner-model.ser.gz"; // TODO make maven
+														 // resolve path
+		String predefinedModel = "all.3class.distsim.crf"; // TODO make maven
+															 // resolve path
+		String typesystemFile = "src/main/resources/typesystem.xml"; // TODO
+																	 // make
+																	 // maven
+																	 // resolve
+																	 // path
 
-		AnalysisEngineDescription ner = createEngineDescription(StanfordNamedEntityRecognizer.class,
-				StanfordNamedEntityRecognizer.PARAM_LANGUAGE, language,
-//				StanfordNamedEntityRecognizer.PARAM_MODEL_LOCATION, modelLocation,
-				StanfordNamedEntityRecognizer.PARAM_VARIANT, "classpath:/de/tudarmstadt/ukp/dkpro/core/stanfordnlp/lib/ner-variants.map",
-				StanfordNamedEntityRecognizer.PARAM_NAMED_ENTITY_MAPPING_LOCATION, "classpath:/de/tudarmstadt/ukp/dkpro/core/stanfordnlp/lib/ner-${language}-${variant}.map");
+		final String LANGUAGECODE_DE = "de";
+		final String LANGUAGECODE_EN = "en";
 
-		AnalysisEngineDescription xmiWriter = createEngineDescription(
-				XmiWriter.class,
-				XmiWriter.PARAM_TARGET_LOCATION, ".",
-				XmiWriter.PARAM_TYPE_SYSTEM_FILE, "typesystem.xml");
+		boolean usePredefinedModel = true;
 
 		/*
-		 * test pipeline - XMI input, NER, XMI output (can be viewed with UIMA
-		 * CAS editor)
+		 * language should be read from document metadata
 		 */
-		runPipeline(reader, ner, xmiWriter);
+		String language = "";
+
+		try {
+			TypeSystemDescription allTypes = mergeBuiltInAndCustomTypes(typesystemFile);
+			for (TypeDescription type : allTypes.getTypes()) {
+				logger.info("Type recognized: " + type.getName());
+			}
+
+			CollectionReaderDescription reader;
+			reader = createReaderDescription(XmiReader.class, allTypes,
+					XmiReader.PARAM_SOURCE_LOCATION, inputPattern,
+					XmiReader.PARAM_LANGUAGE, language);
+
+			AnalysisEngineDescription ner = usePredefinedModel
+					? createEngineDescription(StanfordNamedEntityRecognizer.class,
+							StanfordNamedEntityRecognizer.PARAM_LANGUAGE, language,
+							StanfordNamedEntityRecognizer.PARAM_VARIANT,
+							"classpath:/de/tudarmstadt/ukp/dkpro/core/stanfordnlp/lib/ner-variants.map",
+							StanfordNamedEntityRecognizer.PARAM_NAMED_ENTITY_MAPPING_LOCATION,
+							"classpath:/de/tudarmstadt/ukp/dkpro/core/stanfordnlp/lib/ner-${language}-${variant}.map")
+					: createEngineDescription(StanfordNamedEntityRecognizer.class,
+							StanfordNamedEntityRecognizer.PARAM_LANGUAGE, language,
+							StanfordNamedEntityRecognizer.PARAM_MODEL_LOCATION, modelLocation);
+
+			AnalysisEngineDescription xmiWriter = createEngineDescription(
+					XmiWriter.class,
+					XmiWriter.PARAM_TARGET_LOCATION, ".",
+					XmiWriter.PARAM_TYPE_SYSTEM_FILE, "typesystem.xml");
+
+			/*
+			 * test pipeline - XMI input, NER, XMI output (can be viewed with
+			 * UIMA
+			 * CAS editor)
+			 */
+			runPipeline(reader, ner, xmiWriter);
+
+			/*
+			 * have to create aggregate analysis engine from several aes with
+			 * flow
+			 * controller?
+			 */
+			ConfigurationParameter[] configurationParameters = null;
+			Object[] configurationValues = null;
+			Map<String, ExternalResourceDescription> externalResources = null;
+			FlowControllerDescription flowControllerDescription = FlowControllerFactory.createFlowControllerDescription(
+					FlowController.class, configurationParameters, configurationValues, externalResources);
+
+			List<AnalysisEngineDescription> list = null;
+			List<String> names = null;
+			SofaMapping[] sofaMappings = null;
+			TypePriorities priorities = null;
+
+			AnalysisEngineDescription aggregateEngine = createEngineDescription(list, names, priorities, sofaMappings,
+					flowControllerDescription);
+
+			AnalysisEngine engine = AnalysisEngineFactory.createEngine(aggregateEngine);
+
+		} catch (ResourceInitializationException e) {
+			e.printStackTrace();
+		} catch (UIMAException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
+
+	private static TypeSystemDescription mergeBuiltInAndCustomTypes(String typesystemFile)
+			throws ResourceInitializationException {
+		TypeSystemDescription builtInTypes = TypeSystemDescriptionFactory
+				.createTypeSystemDescription();
+		TypeSystemDescription customTypes = TypeSystemDescriptionFactory
+				.createTypeSystemDescriptionFromPath(typesystemFile);
+		return CasCreationUtils.mergeTypeSystems(Arrays.asList(builtInTypes, customTypes));
+	}
+
 }
