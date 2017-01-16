07a1d216e56996281518d52f8880b05531bcd4c5
neumanmy <mandy.neumann@gesis.org>
2017-01-16 17:06:56 +0100

    #27 Use args4j

--- ss-module-ner/src/main/java/eu/openminted/uc/socialsciences/ner/helper/BinaryCasToStanfordTsvConverter.java
+++ ss-module-ner/src/main/java/eu/openminted/uc/socialsciences/ner/helper/BinaryCasToStanfordTsvConverter.java
@@ -4,86 +4,63 @@ import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDesc
 import static org.apache.uima.fit.factory.CollectionReaderFactory.createReaderDescription;
 import static org.apache.uima.fit.pipeline.SimplePipeline.runPipeline;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.Scanner;
 
 import org.apache.log4j.Logger;
 import org.apache.uima.UIMAException;
-import org.apache.uima.resource.ResourceInitializationException;
+import org.kohsuke.args4j.Option;
+import org.kohsuke.args4j.spi.StringOptionHandler;
 
 import de.tudarmstadt.ukp.dkpro.core.io.bincas.BinaryCasReader;
+import eu.openminted.uc.socialsciences.common.CommandLineArgumentHandler;
 
 public class BinaryCasToStanfordTsvConverter {
 
 	private static final Logger logger = Logger.getLogger(BinaryCasToStanfordTsvConverter.class);
 
-	public static final String LANGUAGE_CODE_EN = "en";
-	public static final String LANGUAGE_CODE_DE = "de";
+	private static final String DEFAULT_LANGUAGE = "en";
+	private static final String DEFAULT_OUTPUT = "stanfordTrain.tsv";
 
+	@Option(name = "-i", handler=StringOptionHandler.class, usage = "input directory containing binary CAS files to be converted", required = true)
 	private String inputPath = null;
-	private String outputPath = null;
-	private String inputLanguage = LANGUAGE_CODE_EN;
+
+	@Option(name = "-o", usage = "[optional] path to save the converted file to")
+	private String outputPath = DEFAULT_OUTPUT;
+
+	@Option(name = "-l", usage = "[optional] language of input")
+	private String inputLanguage = DEFAULT_LANGUAGE;
 
 	public static void main(String[] args) {
-		try {
-			new BinaryCasToStanfordTsvConverter().process(args);
-		} catch (UIMAException | IOException e) {
-			e.printStackTrace();
+		new BinaryCasToStanfordTsvConverter().run(args);
+	}
+
+	protected void run(String[] args) {
+		new CommandLineArgumentHandler().parseInput(args, this);
+
+		logger.info("Reading training file from [" + inputPath + "]");
+
+		if (outputPath.equals(DEFAULT_OUTPUT)) {
+			logger.info("No path for saving the trained model was specified. Default value will be used.");
 		}
-	}
-
-	protected void process(String[] args) throws UIMAException, IOException {
-		processArguments(args);
-
-		convertToTsv(inputPath, outputPath, inputLanguage);
-	}
-
-	private void convertToTsv(String inputResource, String outputResource, String language)
-			throws ResourceInitializationException, UIMAException, IOException {
+		logger.info("Will write the trained model to [" + outputPath + "]");
 
 		/*
 		 * TODO: filter by language, i.e. read language from metadata and create
 		 * output separately for each language!
 		 */
-		runPipeline(
-				createReaderDescription(BinaryCasReader.class,
-						BinaryCasReader.PARAM_SOURCE_LOCATION, inputResource,
-						BinaryCasReader.PARAM_PATTERNS, "/**/*.bin",
-						BinaryCasReader.PARAM_LANGUAGE, language),
-				createEngineDescription(MyStanfordTsvWriter.class,
-						MyStanfordTsvWriter.PARAM_TARGET_LOCATION, outputResource,
-						MyStanfordTsvWriter.PARAM_USE_SUBTYPES, true,
-						MyStanfordTsvWriter.PARAM_SINGULAR_TARGET, true, MyStanfordTsvWriter.PARAM_OVERWRITE, true));
-	}
-
-	// TODO rewrite this using Apache CLI
-	// https://commons.apache.org/proper/commons-cli/usage.html
-	protected void processArguments(String[] args) {
-		if (args.length >= 1) {
-			inputPath = args[0];
+		try {
+			runPipeline(
+					createReaderDescription(BinaryCasReader.class,
+							BinaryCasReader.PARAM_SOURCE_LOCATION, inputPath,
+							BinaryCasReader.PARAM_PATTERNS, "/**/*.bin",
+							BinaryCasReader.PARAM_LANGUAGE, inputLanguage),
+					createEngineDescription(MyStanfordTsvWriter.class,
+							MyStanfordTsvWriter.PARAM_TARGET_LOCATION, outputPath,
+							MyStanfordTsvWriter.PARAM_USE_SUBTYPES, true,
+							MyStanfordTsvWriter.PARAM_SINGULAR_TARGET, true, MyStanfordTsvWriter.PARAM_OVERWRITE,
+							true));
+		} catch (UIMAException | IOException e) {
+			e.printStackTrace();
 		}
-		if (args.length >= 2) {
-			if (args[1].equalsIgnoreCase(LANGUAGE_CODE_DE) || args[1].equalsIgnoreCase(LANGUAGE_CODE_EN)) {
-				inputLanguage = args[1].toLowerCase();
-			} else {
-				logger.warn(
-						"Undefined input language was provided, default value [" + inputLanguage + "] will be used");
-			}
-		} else {
-			logger.warn("Input language was not provided, default value [" + inputLanguage + "] will be used");
-		}
-
-		Scanner scanner = new Scanner(System.in);
-		while (null == inputPath || inputPath.length() < 1) {
-			System.out.println("Please provide path to input directory containing binary CAS files:");
-			inputPath = scanner.nextLine();
-		}
-		scanner.close();
-		outputPath = inputPath + File.separator + "stanfordTrain.tsv";
-
-		logger.info("Input path: " + inputPath);
-		logger.info("Output path: " + outputPath);
-		logger.info("Input language: " + inputLanguage);
 	}
 }
