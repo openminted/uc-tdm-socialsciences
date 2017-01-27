4bb9d4718ccb12fc06f4871b466f0f67a2898b9a
maxxkia <masoud.kiaeeha@gmail.com>
2017-01-27 16:40:29 +0100

    some formatting refactoring

--- ss-io-pdfx-xml/src/main/java/eu/openminted/uc/socialsciences/io/pdfx/CasValidatorComponent.java
+++ ss-io-pdfx-xml/src/main/java/eu/openminted/uc/socialsciences/io/pdfx/CasValidatorComponent.java
@@ -10,26 +10,33 @@ import org.apache.uima.jcas.JCas;
 
 import java.util.List;
 
-public class CasValidatorComponent extends JCasConsumer_ImplBase{
+public class CasValidatorComponent
+        extends JCasConsumer_ImplBase
+{
     private static Logger logger = Logger.getLogger(CasValidatorComponent.class);
     private static CasValidator validator = CasValidator.createWithAllChecks();
 
-    public static final String PARAM_STRICT_CHECK = "false";
+    public static final String PARAM_STRICT_CHECK = "strictCheck";
     @ConfigurationParameter(name = PARAM_STRICT_CHECK, mandatory = false, defaultValue = "false", description =
             "if set to true component will throw an exception when cas contains errors.")
     protected boolean strictCheck;
 
     @Override
-    public void process(JCas aJCas) throws AnalysisEngineProcessException {
+    public void process(JCas aJCas)
+            throws AnalysisEngineProcessException
+    {
         logger.info("CasValidation process started.");
         List<Message> messages = validator.analyze(aJCas);
         logger.info("CasValidation process finished.");
         if (messages.isEmpty())
             logger.info("CasValidation was successful!");
-        else {
+        else
+        {
             logger.warn("CasValidator found " + messages.size() + " issues.");
-            for (Message message:messages){
-                switch (message.level){
+            for (Message message : messages)
+            {
+                switch (message.level)
+                {
                     case INFO:
                         logger.info(message.message);
                         break;
@@ -41,7 +48,7 @@ public class CasValidatorComponent extends JCasConsumer_ImplBase{
                 }
             }
 
-            if(strictCheck)
+            if (strictCheck)
                 throw new IllegalArgumentException(messages.size() + " errors were found in the jcas. You should fix them " +
                         "to be able to run the pipeline.");
         }
