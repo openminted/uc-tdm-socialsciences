081a2ec6f555b7bd36077946a0908732c49f1693
maxxkia <masoud.kiaeeha@gmail.com>
2017-05-28 17:31:15 +0200

    #45 implemented CermineXmlCreator

--- ss-io-pdfx-xml/src/main/java/eu/openminted/uc/socialsciences/io/pdf/cermine/CermineXmlCreator.java
+++ ss-io-pdfx-xml/src/main/java/eu/openminted/uc/socialsciences/io/pdf/cermine/CermineXmlCreator.java
@@ -1,36 +1,44 @@
 package eu.openminted.uc.socialsciences.io.pdf.cermine;
 
-import eu.openminted.uc.socialsciences.io.pdf.XmlCreator;
+import eu.openminted.uc.socialsciences.io.pdfx.AbstractXmlCreator;
+import org.apache.log4j.Logger;
+import org.jdom.Element;
+import org.jdom.output.XMLOutputter;
+import pl.edu.icm.cermine.ContentExtractor;
+import pl.edu.icm.cermine.exception.AnalysisException;
 
+import java.io.*;
 import java.nio.file.Path;
-import java.util.List;
 
 /**
  *
  */
-public class CermineXmlCreator implements XmlCreator
+public class CermineXmlCreator
+        extends AbstractXmlCreator
 {
-    @Override
-    public List<String> getSkippedFileList()
-    {
-        return null;
-    }
+    private static final Logger LOG = Logger.getLogger(CermineXmlCreator.class);
 
     @Override
-    public List<Path> process(String inputPathString, String outputPathString)
+    protected Path singleFileProcess(Path pdfFile, Path outFile)
     {
-        return null;
-    }
-
-    @Override
-    public boolean isOverwriteOutput()
-    {
-        return false;
-    }
-
-    @Override
-    public void setOverwriteOutput(boolean overwriteOutput)
-    {
-
+        Path resultPath = null;
+        ContentExtractor extractor;
+        try {
+            extractor = new ContentExtractor();
+            InputStream inputStream = new FileInputStream(pdfFile.toFile());
+            extractor.setPDF(inputStream);
+            Element result = extractor.getContentAsNLM();
+            OutputStreamWriter outputStream = new OutputStreamWriter(new FileOutputStream(outFile.toFile()), "UTF-8");
+            XMLOutputter outputter = new XMLOutputter();
+            outputter.output(result, outputStream);
+            resultPath = outFile;
+        } catch (AnalysisException e) {
+            LOG.error("AnalysisException was thrown for file [" + pdfFile + "]", e);
+        } catch (FileNotFoundException e) {
+            LOG.error("FileNotFoundException was thrown for file [" + pdfFile + "]", e);
+        } catch (IOException e) {
+            LOG.error("IOException was thrown for file [" + pdfFile + "]", e);
+        }
+        return resultPath;
     }
 }
