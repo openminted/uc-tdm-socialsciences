26811761e9654d4ff6e56f525acd87339dc87f38
maxxkia <masoud.kiaeeha@gmail.com>
2017-10-27 17:40:39 +0200

    #45 - Create Cermine PDF reader
    - apply latest CerminePdfReader changes from DKPro-Core

--- ss-io-pdf/src/test/java/eu/openminted/uc/socialsciences/io/pdf/cermine/CerminePdfReaderTest.java
+++ ss-io-pdf/src/test/java/eu/openminted/uc/socialsciences/io/pdf/cermine/CerminePdfReaderTest.java
@@ -1,40 +1,156 @@
+/**
+ * Copyright 2007-2017
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/ .
+ */
 package eu.openminted.uc.socialsciences.io.pdf.cermine;
 
-import de.tudarmstadt.ukp.dkpro.core.testing.dumper.CasDumpWriter;
-import org.apache.log4j.Logger;
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngine;
+import static org.apache.uima.fit.factory.CollectionReaderFactory.createReader;
+import static org.apache.uima.fit.factory.CollectionReaderFactory.createReaderDescription;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.collection.CollectionReader;
+import org.apache.uima.collection.CollectionReaderDescription;
+import org.apache.uima.fit.pipeline.SimplePipeline;
+import org.apache.uima.jcas.JCas;
+import org.junit.Rule;
 import org.junit.Test;
 
-import static de.tudarmstadt.ukp.dkpro.core.testing.IOTestRunner.testOneWay;
-import static org.apache.uima.fit.factory.CollectionReaderFactory.createReaderDescription;
-import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
-import static org.apache.uima.fit.pipeline.SimplePipeline.runPipeline;
+import de.tudarmstadt.ukp.dkpro.core.api.metadata.type.DocumentMetaData;
+import de.tudarmstadt.ukp.dkpro.core.testing.DkproTestContext;
+import de.tudarmstadt.ukp.dkpro.core.testing.dumper.CasDumpWriter;
+
 
 public class CerminePdfReaderTest
 {
-    private static final Logger logger = Logger.getLogger(CerminePdfReader.class);
+    @Test
+    public void test() throws Exception
+    {
+        File outputFile = new File(testContext.getTestOutputFolder(), "dump-output.txt");
+
+        CollectionReader reader = createReader(CerminePdfReader.class,
+                CerminePdfReader.PARAM_SOURCE_LOCATION, "src/test/resources/data",
+                CerminePdfReader.PARAM_PATTERNS, "[+]**/*.pdf");
+
+        AnalysisEngine writer = createEngine(CasDumpWriter.class,
+                CasDumpWriter.PARAM_TARGET_LOCATION, outputFile);
+
+        SimplePipeline.runPipeline(reader, writer);
+
+        assertTrue(FileUtils.contentEqualsIgnoreEOL(
+                new File("src/test/resources/reference/test.dump"),
+                outputFile, 
+                "UTF-8"));
+    }
 
     @Test
-    public void testReadArticle() throws Exception
+    public void testMetadata() throws Exception
     {
-            String filePath = "src/test/resources/42466_Paper.pdf";
-            String expectedFilePath = filePath + ".cermine.dump";
+        List<String> expectedTitles = new ArrayList<>();
+        expectedTitles.add("Out-of-domain FrameNet Semantic Role Labeling");
 
-/*
-			runPipeline(
-					createReaderDescription(CerminePdfReader.class,
-							CerminePdfReader.PARAM_LANGUAGE, "en",
-							CerminePdfReader.PARAM_SOURCE_LOCATION, filePath),
-					createEngineDescription(CasDumpWriter.class,
-							CasDumpWriter.PARAM_TARGET_LOCATION, expectedFilePath,
-							CasDumpWriter.PARAM_SORT, true));
-*/
+        CollectionReaderDescription reader = createReaderDescription(CerminePdfReader.class,
+                CerminePdfReader.PARAM_SOURCE_LOCATION, "src/test/resources/data",
+                CerminePdfReader.PARAM_PATTERNS, "[+]**/*.pdf");
 
+        List<String> actualTitles = new ArrayList<>();
+        for (JCas jcas : SimplePipeline.iteratePipeline(reader)) {
+            DocumentMetaData metadata = DocumentMetaData.get(jcas);
+            actualTitles.add(metadata.getDocumentTitle());
+        }
 
-            String fileName = "42466_Paper.pdf";
-            String expectedFileName = fileName + ".cermine.dump";
-            logger.info("Checking reader on file [" + fileName + "] against the dump file [" + expectedFileName + "]");
-
-            testOneWay(createReaderDescription(CerminePdfReader.class, CerminePdfReader.PARAM_LANGUAGE, "en"),
-                    expectedFileName, fileName);
+        for (String expectedTitle : expectedTitles) {
+            assertTrue(actualTitles.contains(expectedTitle));
+            actualTitles.remove(expectedTitle);
+        }
     }
+    
+    @Test
+    public void testNormalizeText() throws Exception
+    {
+        File outputFile = new File(testContext.getTestOutputFolder(), "dump-output.txt");
+
+        CollectionReader reader = createReader(CerminePdfReader.class,
+                CerminePdfReader.PARAM_SOURCE_LOCATION, "src/test/resources/data",
+                CerminePdfReader.PARAM_PATTERNS, "[+]**/*.pdf",
+                CerminePdfReader.PARAM_NORMALIZE_TEXT, true);
+
+        AnalysisEngine writer = createEngine(CasDumpWriter.class,
+                CasDumpWriter.PARAM_TARGET_LOCATION, outputFile);
+
+        SimplePipeline.runPipeline(reader, writer);
+
+        assertTrue(FileUtils.contentEqualsIgnoreEOL(
+                new File("src/test/resources/reference/test-normalized.dump"),
+                outputFile, 
+                "UTF-8"));
+    }
+    
+    @Test
+    public void testIgnoreCitations() throws Exception
+    {
+        File outputFile = new File(testContext.getTestOutputFolder(), "dump-output.txt");
+
+        CollectionReader reader = createReader(CerminePdfReader.class,
+                CerminePdfReader.PARAM_SOURCE_LOCATION, "src/test/resources/data",
+                CerminePdfReader.PARAM_PATTERNS, "[+]**/*.pdf",
+                CerminePdfReader.PARAM_NORMALIZE_TEXT, true, CerminePdfReader.PARAM_IGNORE_CITATIONS,
+                true);
+
+        AnalysisEngine writer = createEngine(CasDumpWriter.class,
+                CasDumpWriter.PARAM_TARGET_LOCATION, outputFile);
+
+        SimplePipeline.runPipeline(reader, writer);
+
+        assertTrue(FileUtils.contentEqualsIgnoreEOL(
+                new File("src/test/resources/reference/test-normalized-nocitations.dump"),
+                outputFile, 
+                "UTF-8"));
+    }
+    
+    @Test
+    public void testIgnoreReferencesSection() throws Exception
+    {
+        File outputFile = new File(testContext.getTestOutputFolder(), "dump-output.txt");
+
+        CollectionReader reader = createReader(CerminePdfReader.class,
+                CerminePdfReader.PARAM_SOURCE_LOCATION, "src/test/resources/data",
+                CerminePdfReader.PARAM_PATTERNS, "[+]**/*.pdf",
+                CerminePdfReader.PARAM_NORMALIZE_TEXT, true,
+                CerminePdfReader.PARAM_IGNORE_CITATIONS, true,
+                CerminePdfReader.PARAM_IGNORE_REFERENCES_SECTION, true);
+
+        AnalysisEngine writer = createEngine(CasDumpWriter.class,
+                CasDumpWriter.PARAM_TARGET_LOCATION, outputFile);
+
+        SimplePipeline.runPipeline(reader, writer);
+
+        assertTrue(FileUtils.contentEqualsIgnoreEOL(
+                new File("src/test/resources/reference/test-normalized-nocitations-noRefSection.dump"),
+                outputFile, 
+                "UTF-8"));
+    }
+
+    @Rule
+    public DkproTestContext testContext = new DkproTestContext();
 }
