4e80619e630bb27fc1ad8a0354aa50de3e8ebadb
maxxkia <masoud.kiaeeha@gmail.com>
2017-10-27 15:39:20 +0200

    No issue - performed refactoring

--- ss-variable-detection/src/main/java/eu/openminted/uc/socialsciences/variabledetection/TrainTestPipeline.java
+++ ss-variable-detection/src/main/java/eu/openminted/uc/socialsciences/variabledetection/TrainTestPipeline.java
@@ -4,7 +4,6 @@ import static java.util.Arrays.asList;
 import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
 
 import java.io.File;
-import java.net.URISyntaxException;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -46,6 +45,11 @@ import weka.classifiers.functions.supportVector.PolyKernel;
 import weka.classifiers.meta.Bagging;
 import weka.classifiers.trees.J48;
 
+/**
+ * Pipeline for training a machine learning model using the training data and evaluating the model
+ * on test data. The pipeline uses several textual features to train a model using
+ * <a href="https://github.com/dkpro/dkpro-tc">DKPro-TC</a>.
+ */
 public class TrainTestPipeline
     implements Constants
 {
@@ -68,8 +72,89 @@ public class TrainTestPipeline
         experiment.runTrainTest(pSpace);
     }
 
-    @SuppressWarnings("unchecked")
+    /**
+     * Creates the the parameter space ({@link ParameterSpace}) for the experiment. The parameter
+     * space describes the experiment input(s), classifier(s), feature extractor(s), and feature selector(s).
+     */
     public static ParameterSpace getParameterSpace() throws ResourceInitializationException
+    {
+        
+        Dimension<Map<String, Object>> dimReaders = createReadersDimension();
+        Dimension<List<String>> dimClassificationArgs = createClassifiersDimension();
+        Dimension<TcFeatureSet> dimFeatureSets = createFeatureExtractorsDimension();
+        Dimension<Map<String, Object>> dimFeatureSelection = createFeatureSelectionDimension();
+
+        ParameterSpace pSpace = new ParameterSpace(dimReaders,
+                Dimension.create(DIM_LEARNING_MODE, LM_SINGLE_LABEL),
+                Dimension.create(DIM_FEATURE_MODE, FM_DOCUMENT), dimFeatureSets,
+                dimClassificationArgs, dimFeatureSelection);
+
+        return pSpace;
+    }
+
+    private static Dimension<Map<String, Object>> createFeatureSelectionDimension()
+    {
+        // single-label feature selection (Weka specific options), reduces the feature set to N
+        Map<String, Object> dimFeatureSelection = new HashMap<String, Object>();
+        dimFeatureSelection.put(DIM_FEATURE_SEARCHER_ARGS,
+                asList(new String[] { Ranker.class.getName(), "-N", "100" }));
+        dimFeatureSelection.put(DIM_ATTRIBUTE_EVALUATOR_ARGS,
+                asList(new String[] { InfoGainAttributeEval.class.getName() }));
+        dimFeatureSelection.put(DIM_APPLY_FEATURE_SELECTION, true);
+        return Dimension.createBundle("featureSelection", dimFeatureSelection);
+    }
+
+    private static Dimension<TcFeatureSet> createFeatureExtractorsDimension()
+    {
+        return Dimension.create(DIM_FEATURE_SET, new TcFeatureSet(
+                TcFeatureFactory.create(LuceneNGram.class, LuceneNGram.PARAM_NGRAM_USE_TOP_K, 50,
+                        LuceneNGram.PARAM_NGRAM_MIN_N, 1, LuceneNGram.PARAM_NGRAM_MAX_N, 3),
+                TcFeatureFactory.create(LuceneLemmaNGram.class,
+                        LuceneLemmaNGram.PARAM_NGRAM_USE_TOP_K, 50,
+                        LuceneLemmaNGram.PARAM_NGRAM_MIN_N, 2, LuceneLemmaNGram.PARAM_NGRAM_MAX_N,
+                        3),
+//                TcFeatureFactory.create(LuceneCharacterNGram.class,
+//                        LuceneCharacterNGram.PARAM_NGRAM_USE_TOP_K, 50,
+//                        LuceneCharacterNGram.PARAM_NGRAM_MIN_N, 1,
+//                        LuceneCharacterNGram.PARAM_NGRAM_MAX_N, 3),
+                // TcFeatureFactory.create(LucenePhoneticNGram.class,
+                // LucenePhoneticNGram.PARAM_NGRAM_USE_TOP_K, 50,
+                // LucenePhoneticNGram.PARAM_NGRAM_MIN_N, 1,
+                // LucenePhoneticNGram.PARAM_NGRAM_MAX_N, 3),
+                TcFeatureFactory.create(LuceneSkipNGram.class,
+                        LuceneSkipNGram.PARAM_NGRAM_USE_TOP_K, 50,
+                        LuceneSkipNGram.PARAM_NGRAM_MIN_N, 2, LuceneSkipNGram.PARAM_NGRAM_MAX_N, 3),
+                TcFeatureFactory.create(NEFeatureExtractor.class),
+                TcFeatureFactory.create(WordnetFeatures.class, WordnetFeatures.PARAM_RESOURCE_NAME,
+                        WordnetFeatures.WORDNET_FIELD, WordnetFeatures.PARAM_RESOURCE_LANGUAGE, "en",
+                        WordnetFeatures.PARAM_NGRAM_MIN_N, 1,
+                        WordnetFeatures.PARAM_NGRAM_MAX_N, 4,
+                        WordnetFeatures.PARAM_NGRAM_USE_TOP_K, Integer.MAX_VALUE,
+                        WordnetFeatures.PARAM_SYNONYM_FEATURE, true,
+                        WordnetFeatures.PARAM_HYPERNYM_FEATURE, false),
+                TcFeatureFactory.create(TheSozFeatures.class,
+                        TheSozFeatures.PARAM_RESOURCE_NAME, TheSozResource.NAME,
+                        TheSozFeatures.PARAM_NGRAM_MIN_N, 1,
+                        TheSozFeatures.PARAM_NGRAM_MAX_N, 3)));
+    }
+
+    @SuppressWarnings("unchecked")
+    private static Dimension<List<String>> createClassifiersDimension()
+    {
+        // We configure 3 different classifiers, which will be swept, each with a special
+        // configuration.
+        return Dimension.create(DIM_CLASSIFICATION_ARGS,
+                // "-C": complexity, "-K": kernel
+                asList(new String[] { SMO.class.getName(), "-C", "1.0", "-K",
+                        PolyKernel.class.getName() + " " + "-C -1 -E 2" }),
+                asList(new String[] { NaiveBayes.class.getName(), "-K" }),
+                // "W": base classifier
+                asList(new String[] { Bagging.class.getName(), "-I", "2", "-W", J48.class.getName(),
+                        "--", "-C", "0.5", "-M", "2" }));
+    }
+
+    private static Dimension<Map<String, Object>> createReadersDimension()
+        throws ResourceInitializationException
     {
         // configure training and test data reader dimension
         Map<String, Object> dimReaders = new HashMap<String, Object>();
@@ -87,62 +172,7 @@ public class TrainTestPipeline
                 TextDatasetReader.PARAM_PATTERNS,
                 Arrays.asList(TextDatasetReader.INCLUDE_PREFIX + "**/*.txt"));
         dimReaders.put(DIM_READER_TEST, readerTest);
-
-        // We configure 3 different classifiers, which will be swept, each with a special
-        // configuration.
-        Dimension<List<String>> dimClassificationArgs = Dimension.create(DIM_CLASSIFICATION_ARGS,
-                // "-C": complexity, "-K": kernel
-                asList(new String[] { SMO.class.getName(), "-C", "1.0", "-K",
-                        PolyKernel.class.getName() + " " + "-C -1 -E 2" }),
-                asList(new String[] { NaiveBayes.class.getName(), "-K" }),
-                // "W": base classifier
-                asList(new String[] { Bagging.class.getName(), "-I", "2", "-W", J48.class.getName(),
-                        "--", "-C", "0.5", "-M", "2" }));
-
-        // We configure 1 set of feature extractors, consisting of 1 extractor
-        Dimension<TcFeatureSet> dimFeatureSets = Dimension.create(DIM_FEATURE_SET, new TcFeatureSet(
-                TcFeatureFactory.create(LuceneNGram.class, LuceneNGram.PARAM_NGRAM_USE_TOP_K, 50,
-                        LuceneNGram.PARAM_NGRAM_MIN_N, 1, LuceneNGram.PARAM_NGRAM_MAX_N, 3),
-                TcFeatureFactory.create(LuceneLemmaNGram.class,
-                        LuceneLemmaNGram.PARAM_NGRAM_USE_TOP_K, 50,
-                        LuceneLemmaNGram.PARAM_NGRAM_MIN_N, 2, LuceneLemmaNGram.PARAM_NGRAM_MAX_N,
-                        3),
-                TcFeatureFactory.create(LuceneCharacterNGram.class,
-                        LuceneCharacterNGram.PARAM_NGRAM_USE_TOP_K, 50,
-                        LuceneCharacterNGram.PARAM_NGRAM_MIN_N, 1,
-                        LuceneCharacterNGram.PARAM_NGRAM_MAX_N, 3),
-                // TcFeatureFactory.create(LucenePhoneticNGram.class,
-                // LucenePhoneticNGram.PARAM_NGRAM_USE_TOP_K, 50,
-                // LucenePhoneticNGram.PARAM_NGRAM_MIN_N, 1,
-                // LucenePhoneticNGram.PARAM_NGRAM_MAX_N, 3),
-                TcFeatureFactory.create(LuceneSkipNGram.class,
-                        LuceneSkipNGram.PARAM_NGRAM_USE_TOP_K, 50,
-                        LuceneSkipNGram.PARAM_NGRAM_MIN_N, 2, LuceneSkipNGram.PARAM_NGRAM_MAX_N, 3),
-                TcFeatureFactory.create(NEFeatureExtractor.class),
-                TcFeatureFactory.create(WordnetFeatures.class, WordnetFeatures.PARAM_RESOURCE_NAME,
-                        WordnetFeatures.WORDNET_FIELD, WordnetFeatures.PARAM_RESOURCE_LANGUAGE, "en",
-                        WordnetFeatures.PARAM_SYNONYM_FEATURE, true,
-                        WordnetFeatures.PARAM_HYPERNYM_FEATURE, false),
-                TcFeatureFactory.create(TheSozFeatures.class,
-                        TheSozFeatures.PARAM_RESOURCE_NAME, TheSozResource.NAME,
-                        TheSozFeatures.PARAM_NGRAM_MIN_N, 1,
-                        TheSozFeatures.PARAM_NGRAM_MAX_N, 3)));
-
-        // single-label feature selection (Weka specific options), reduces the feature set to N
-        Map<String, Object> dimFeatureSelection = new HashMap<String, Object>();
-        dimFeatureSelection.put(DIM_FEATURE_SEARCHER_ARGS,
-                asList(new String[] { Ranker.class.getName(), "-N", "100" }));
-        dimFeatureSelection.put(DIM_ATTRIBUTE_EVALUATOR_ARGS,
-                asList(new String[] { InfoGainAttributeEval.class.getName() }));
-        dimFeatureSelection.put(DIM_APPLY_FEATURE_SELECTION, true);
-
-        ParameterSpace pSpace = new ParameterSpace(Dimension.createBundle("readers", dimReaders),
-                Dimension.create(DIM_LEARNING_MODE, LM_SINGLE_LABEL),
-                Dimension.create(DIM_FEATURE_MODE, FM_DOCUMENT), dimFeatureSets,
-                dimClassificationArgs,
-                Dimension.createBundle("featureSelection", dimFeatureSelection));
-
-        return pSpace;
+        return Dimension.createBundle("readers", dimReaders);
     }
 
     protected void runTrainTest(ParameterSpace pSpace) throws Exception
