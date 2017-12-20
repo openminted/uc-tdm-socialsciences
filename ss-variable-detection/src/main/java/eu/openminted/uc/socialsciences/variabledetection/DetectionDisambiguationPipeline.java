8a54bca046f3e689a912a8e177e931b1496e254d
maxxkia <masoud.kiaeeha@gmail.com>
2017-12-20 15:45:32 +0100

    #62 - Implement variable detector disambiguator component
    - added model paths to DetectionDisambiguationPipeline

--- ss-variable-detection/src/main/java/eu/openminted/uc/socialsciences/variabledetection/DetectionDisambiguationPipeline.java
+++ ss-variable-detection/src/main/java/eu/openminted/uc/socialsciences/variabledetection/DetectionDisambiguationPipeline.java
@@ -23,14 +23,13 @@ import eu.openminted.uc.socialsciences.variabledetection.io.XmlCorpusAllDocsRead
 
 public class DetectionDisambiguationPipeline
 {
+    private static final String DETECTION_MODEL_LOCATION = "/home/local/UKP/kiaeeha/git/uc-tdm-socialsciences/data/models/variable-detection/";
+    private static final String DISAMBIGUATION_MODEL_LOCATION = "/home/local/UKP/kiaeeha/git/uc-tdm-socialsciences/data/models/variable-disambiguation/variable-disambiguation-model.ser";
     private static final String COPRUS_FILEPATH_TEST = "/home/local/UKP/kiaeeha/workspace/Datasets"
             + "/openminted/uc-ss/variable-detection/detection/Full_ALLDOCS.xml";
     private static final String LANGUAGE_CODE = "en";
     public static final File PREDICTION_PATH = new File("target/prediction");
 
-    /**
-     * Starts the experiment.
-     */
     public static void main(String[] args) throws Exception
     {
         DetectionDisambiguationPipeline experiment = new DetectionDisambiguationPipeline();
@@ -53,8 +52,10 @@ public class DetectionDisambiguationPipeline
                 createEngineDescription(OpenNlpNamedEntityRecognizer.class),
                 createEngineDescription(StopWordRemover.class,
                         StopWordRemover.PARAM_MODEL_LOCATION, getClass().getResource("/stopwords/english.txt").toString()),
-                createEngineDescription(VariableMentionDetector.class),
-                createEngineDescription(VariableMentionDisambiguator.class),
+                createEngineDescription(VariableMentionDetector.class,
+                        VariableMentionDetector.PARAM_MODEL_LOCATION, DETECTION_MODEL_LOCATION),
+                createEngineDescription(VariableMentionDisambiguator.class,
+                        VariableMentionDisambiguator.PARAM_MODEL_LOCATION, DISAMBIGUATION_MODEL_LOCATION),
                 createEngineDescription(XmiWriter.class,
                         XmiWriter.PARAM_TARGET_LOCATION, PREDICTION_PATH,
                         XmiWriter.PARAM_OVERWRITE, true));
