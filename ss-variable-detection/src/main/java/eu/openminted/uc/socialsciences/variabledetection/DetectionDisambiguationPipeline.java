edc3a6496da4942850e36728d5bb9e42e58ea2ac
Richard Eckart de Castilho <richard.eckart@gmail.com>
2018-03-03 17:45:45 +0100

    No issue. Formatting. Include gold annotations for evaluation. Disambiguate all mentions irrespective of what the mention detector says.

--- ss-variable-detection/src/main/java/eu/openminted/uc/socialsciences/variabledetection/DetectionDisambiguationPipeline.java
+++ ss-variable-detection/src/main/java/eu/openminted/uc/socialsciences/variabledetection/DetectionDisambiguationPipeline.java
@@ -1,15 +1,10 @@
 package eu.openminted.uc.socialsciences.variabledetection;
 
 import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
+import static org.apache.uima.fit.factory.CollectionReaderFactory.createReaderDescription;
+import static org.apache.uima.fit.pipeline.SimplePipeline.runPipeline;
 
 import java.io.File;
-import java.io.IOException;
-
-import org.apache.uima.UIMAException;
-import org.apache.uima.collection.CollectionReaderDescription;
-import org.apache.uima.fit.factory.CollectionReaderFactory;
-import org.apache.uima.fit.pipeline.SimplePipeline;
-import org.apache.uima.resource.ResourceInitializationException;
 
 import de.tudarmstadt.ukp.dkpro.core.io.xmi.XmiWriter;
 import de.tudarmstadt.ukp.dkpro.core.opennlp.OpenNlpNamedEntityRecognizer;
@@ -27,27 +22,19 @@ public class DetectionDisambiguationPipeline
     private static final String DISAMBIGUATION_MODEL_LOCATION = "../data/models/variable-disambiguation/variable-disambiguation-model.ser";
     private static final String COPRUS_FILEPATH_TEST = "../data/datasets/Full_ALLDOCS_English.xml";
     private static final String LANGUAGE_CODE = "en";
-    public static final File PREDICTION_PATH = new File("target/prediction");
+    private static final File PREDICTION_PATH = new File("target/prediction");
 
     public static void main(String[] args) throws Exception
     {
         // Route logging through log4j
         System.setProperty("org.apache.uima.logger.class", "org.apache.uima.util.impl.Log4jLogger_impl");
-        
-        DetectionDisambiguationPipeline experiment = new DetectionDisambiguationPipeline();
-        experiment.run();
-    }
 
-    protected void run()
-        throws ResourceInitializationException, UIMAException, IOException
-    {
-        CollectionReaderDescription reader = CollectionReaderFactory.createReaderDescription(
-                XmlCorpusAllDocsReader.class, 
-                XmlCorpusAllDocsReader.PARAM_SOURCE_LOCATION, COPRUS_FILEPATH_TEST, 
-                XmlCorpusAllDocsReader.PARAM_LANGUAGE, LANGUAGE_CODE);
-        
-        SimplePipeline.runPipeline(
-                reader,
+        runPipeline(
+                createReaderDescription(
+                        XmlCorpusAllDocsReader.class, 
+                        XmlCorpusAllDocsReader.PARAM_INCLUDE_GOLD, true,
+                        XmlCorpusAllDocsReader.PARAM_SOURCE_LOCATION, COPRUS_FILEPATH_TEST, 
+                        XmlCorpusAllDocsReader.PARAM_LANGUAGE, LANGUAGE_CODE),
                 //Preprocessing should be the same as the one used for model training
                 createEngineDescription(BreakIteratorSegmenter.class),
                 createEngineDescription(OpenNlpPosTagger.class),
@@ -58,6 +45,7 @@ public class DetectionDisambiguationPipeline
                 createEngineDescription(VariableMentionDetector.class,
                         VariableMentionDetector.PARAM_MODEL_LOCATION, DETECTION_MODEL_LOCATION),
                 createEngineDescription(VariableMentionDisambiguator.class,
+                        VariableMentionDisambiguator.PARAM_DISAMBIGUATE_ALL_MENTIONS, true,
                         VariableMentionDisambiguator.PARAM_MODEL_LOCATION, DISAMBIGUATION_MODEL_LOCATION,
                         VariableMentionDisambiguator.PARAM_VARIABLE_FILE_LOCATION, "../data/datasets/Variables_English.xml"),
                 createEngineDescription(XmiWriter.class,
